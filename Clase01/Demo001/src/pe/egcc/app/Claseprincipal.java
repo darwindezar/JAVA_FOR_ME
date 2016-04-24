69a71609084780fd157142ad8bdaed88b8a65837
Darwin Deza Ricopa <darwindezar@hotmail.com>
2016-04-24 17:37:58 -0500

    Primera Clase

---
+++ Clase01/Demo001/src/pe/egcc/app/Claseprincipal.java
@@ -0,0 +1,14 @@
+
+package pe.egcc.app;
+
+public class Claseprincipal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+       
+    }
+    
+}
